410d873cf4ef7d8c6569e654581a0a4210e63863
Björn Guth <bjoern.guth@rwth-aachen.de>
2016-09-30 22:11:15 +0200

    adding files, commiting and pushing now implemented

--- photobot.py
+++ photobot.py
@@ -3,6 +3,7 @@
 import config
 from git import Repo, RemoteProgress
 import os
+import datetime
 
 class MyProgressPrinter(RemoteProgress):
 	def update(self, op_code, cur_count, max_count=None, message=''):
@@ -16,3 +17,33 @@ if os.path.isdir(config.data_dir):
 			print("Updated %s to %s" % (fetch_info.ref, fetch_info.commit))
 else:
 	data_repo = Repo.clone_from(config.data_repo_url, config.data_dir, progress=MyProgressPrinter())
+
+data_file = None
+with open(os.path.join(config.data_dir, 'test'), 'r') as test_file:
+	data_file = test_file.read()
+
+data_list = data_file.split('\n')
+
+time = datetime.datetime.now()
+testout_path = os.path.join(data_repo.working_tree_dir, 'testout')
+
+out_file = None
+if os.path.isfile(testout_path):
+	with open(testout_path, 'r') as testout_file:
+		out_file = testout_file.read()
+else:
+	out_file = ''
+
+out_list = out_file.split('\n')
+out_list.append(data_list[-2])
+out_file = '\n'.join(out_list)
+
+with open(testout_path, 'w') as testout_file:
+	testout_file.write(out_file)
+
+print(data_repo.working_tree_dir)
+data_repo.index.add(['testout'])
+data_repo.index.commit('testcommit from ' + str(time))
+data_repo.remote().push()
+
+
