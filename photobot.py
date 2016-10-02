ecffa830e0f5408085ff143b6dd49a1645667704
Björn Guth <bjoern.guth@rwth-aachen.de>
2016-10-03 01:19:55 +0200

    removed code to test git

--- photobot.py
+++ photobot.py
@@ -13,37 +13,12 @@ if os.path.isdir(config.data_dir):
 	data_repo = Repo(config.data_dir)
 	if not data_repo.is_dirty():
 		remote = data_repo.remote()
-		for fetch_info in remote.pull(progress=MyProgressPrinter()):
+		for fetch_info in remote.pull():
 			print("Updated %s to %s" % (fetch_info.ref, fetch_info.commit))
 else:
-	data_repo = Repo.clone_from(config.data_repo_url, config.data_dir, progress=MyProgressPrinter())
-
-data_file = None
-with open(os.path.join(config.data_dir, 'test'), 'r') as test_file:
-	data_file = test_file.read()
-
-data_list = data_file.split('\n')
-
-time = datetime.datetime.now()
-testout_path = os.path.join(data_repo.working_tree_dir, 'testout')
-
-out_file = None
-if os.path.isfile(testout_path):
-	with open(testout_path, 'r') as testout_file:
-		out_file = testout_file.read()
-else:
-	out_file = ''
-
-out_list = out_file.split('\n')
-out_list.append(data_list[-2])
-out_file = '\n'.join(out_list)
-
-with open(testout_path, 'w') as testout_file:
-	testout_file.write(out_file)
-
-print(data_repo.working_tree_dir)
-data_repo.index.add(['testout'])
-data_repo.index.commit('testcommit from ' + str(time))
-data_repo.remote().push()
+	data_repo = Repo.clone_from(config.data_repo_url, config.data_dir)
 
 
+# data_repo.index.add(['testout'])
+# data_repo.index.commit('testcommit from ' + str(time))
+# data_repo.remote().push()
