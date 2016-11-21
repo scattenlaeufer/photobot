56b5b2b0bbf1454f5d1ee3be7caf0e9dd9701299
Björn Guth <bjoern.guth@rwth-aachen.de>
2016-11-21 18:51:42 +0100

    i now get a oauth_token and oauth_token_secret for api.500px.com

--- photobot.py
+++ photobot.py
@@ -2,8 +2,10 @@
 
 import config
 from git import Repo, RemoteProgress
+from fivehundredpx.auth import OAuthHandler
 import os
 import datetime
+import oauth2
 
 class MyProgressPrinter(RemoteProgress):
 	def update(self, op_code, cur_count, max_count=None, message=''):
@@ -17,6 +19,17 @@ if os.path.isdir(config.data_dir):
 			print("Updated %s to %s" % (fetch_info.ref, fetch_info.commit))
 else:
 	data_repo = Repo.clone_from(config.data_repo_url, config.data_dir)
+
+handler_500px = OAuthHandler(config.consumer_key, config.consumer_secret)
+#help(handler_500px)
+#token = handler_500px.get_request_token()
+
+consumer = oauth2.Consumer(config.consumer_key, config.consumer_secret)
+client = oauth2.Client(consumer)
+resp, content = client.request('https://api.500px.com/v1/oauth/request_token', 'GET')
+
+print(resp)
+print(content)
 
 
 # data_repo.index.add(['testout'])
